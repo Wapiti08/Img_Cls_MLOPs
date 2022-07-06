4909acb123f507c607d1648708bf17778a6d69f6
tzrzhuoran@gmail.com <tzrzhuoran@gmail.com>
2022-07-06 17:15:08 +0100

    building main.py

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /app
 
 # COPY files
 COPY app.py /app
+COPy main.py /main.py
 COPY requirements.txt /app
 COPY model /app/model
 COPY ms /app/ms
@@ -14,5 +15,6 @@ RUN pip3 install -r requirements.txt
 
 # Run the application
 EXPOSE 8000
-ENTRYPOINT [  "gunicorn", "-b", "0.0.0.0:8000", "--access-logfile", "-", "--error-logfile", "-", "--timeout", "120"]
-CMD ["app:app"]
+# ENTRYPOINT [  "gunicorn", "-b", "0.0.0.0:8000", "--access-logfile", "-", "--error-logfile", "-", "--timeout", "120"]
+CMD [  "uvicorn", "main.py", "-b", "0.0.0.0:8000"]
+# CMD ["app:app"]
--- main.py
+++ main.py
@@ -10,6 +10,47 @@ version = 'v1.0.0'
 
 # data validation for Input
 class Input(BaseModel):
-    concavity_mean: float = Field(..., gt=0)
+    concavity_mean: float
+    concave_points_mean: float
+    perimeter_se: float
+    area_se: float
+    texture_worst: float
+    area_worst: float
+
+    class Config:
+        schema_extra = {
+             "concavity_mean": 0.3001,
+            "concave_points_mean": 0.1471,
+            "perimeter_se": 8.589,
+            "area_se": 153.4,
+            "texture_worst": 17.33,
+            "area_worst": 2019.0,
+        }
+
 
 # data validation for output
+class Output(BaseModel):
+    label: str
+    prediction: int
+
+@app.route('/info', method=['GET'])
+async def model_info():
+    model_info = {}
+    model_info['version'] = version
+    model_info['model_name'] = model_name
+
+    return model_info
+
+
+@app.route('/health', method=['GET'])
+async def service_health():
+    '''
+    
+    ''' 
+    return 'ok'
+
+@app.route('/predict', method=['POST'])
+async def predict(input):
+    response = get_model_response(input)
+
+    return response
